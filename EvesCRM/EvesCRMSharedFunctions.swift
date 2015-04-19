5abc61a793dec5920c886439c689792e9e8b0781
garryeves <garry@eves.id.au>
2015-04-19 11:58:21 +0800

    Commit due to issues with project file

---
+++ EvesCRM/EvesCRMSharedFunctions.swift
@@ -0,0 +1,15 @@
+//
+//  EvesCRMSharedFunctions.swift
+//  EvesCRM
+//
+//  Created by Garry Eves on 19/04/2015.
+//  Copyright (c) 2015 Garry Eves. All rights reserved.
+//
+
+import Foundation
+
+func writeRowToArray(inStr: String, inout inTable: [String])
+{
+    
+    inTable.append(inStr)
+}
